7d133e4b0fbd7683ad62784d44c29277b8190ed2
Alan Low <30099983+alanlowzies@users.noreply.github.com>
2018-07-04 01:50:06 +0800

    Add files via upload

---
+++ Numbers/Find Pi to the Nth Digit/Pi.java
@@ -0,0 +1,9 @@
+
+public class Pi {
+	//Class to store the value of pi and a getter to retrieve pi... that's about it... lul
+	private double pi = Math.PI;
+	
+	public double getPI() {
+		return this.pi;
+	}
+}
